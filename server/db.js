7bc83701d94839bb1be8919267f41af4c03d04f8
MouhaDiouf <mouhamadoudioufsn@gmail.com>
2023-12-31 20:25:26 +0000

    update db

--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://root:MTY3MDEtbW91aGFt@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
     const count = retryCount ?? 0;
     try {
-        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
-        console.info('Connected to Mongo Successfully')
+        await mongoose.connect(mongoURI, { dbName: "stayhealthybeta1" });
+        console.info("Connected to Mongo Successfully");
 
         return;
     } catch (error) {
@@ -15,13 +15,12 @@ const connectToMongo = async (retryCount) => {
         const nextRetryCount = count + 1;
 
         if (nextRetryCount >= MAX_RETRIES) {
-            throw new Error('Unable to connect to Mongo!');
+            throw new Error("Unable to connect to Mongo!");
         }
 
-        console.info(`Retrying, retry count: ${nextRetryCount}`)
+        console.info(`Retrying, retry count: ${nextRetryCount}`);
 
         return await connectToMongo(nextRetryCount);
-
     }
 };
 
