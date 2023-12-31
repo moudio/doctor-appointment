4b6bc88858dc940c274925b29f06e64aeee99963
Mouha <moudiou@outlook.com>
2023-12-31 15:35:10 -0500

    update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const API_URL = window.location.hostname === "localhost" ? "<add your theia server side url>" : "add your theia server side url";
+export const API_URL = window.location.hostname === "localhost" ? "https://08eb7241-5769-4620-b196-86a24c12e0d4.mini-browser.theiadockernext-1-labs-prod-theiak8s-4-tor01.labs.cognitiveclass.ai/user/mouhamadoud3/mini-browser/url" : "https://08eb7241-5769-4620-b196-86a24c12e0d4.mini-browser.theiadockernext-1-labs-prod-theiak8s-4-tor01.labs.cognitiveclass.ai/user/mouhamadoud3/mini-browser/url";
 console.log(
     "API_URL :",
     API_URL
